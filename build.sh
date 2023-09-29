6cb0508f30f6472d45261b5a7bef71b60d1670c6
Ilya Kreymer <ikreymer@gmail.com>
2023-09-29 09:42:27 -0700

    bump brave version to 1.58.135

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 export BROWSER=brave
 export BROWSER_VERSION=latest
 # build just for local platform
-docker buildx build --target=$BROWSER --build-arg BROWSER=$BROWSER --build-arg BROWSER_VERSION=$BROWSER_VERSION --progress simple --load -t webrecorder/browsertrix-browser-base:${BROWSER}-${BROWSER_VERSION} .
+docker buildx build --target=$BROWSER --build-arg BROWSER=$BROWSER --build-arg BROWSER_VERSION=$BROWSER_VERSION --progress plain --load -t webrecorder/browsertrix-browser-base:${BROWSER}-${BROWSER_VERSION} .
 
