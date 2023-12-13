098300ddd765cf39797143967c2eb6fe8135248f
Ilya Kreymer <ikreymer@gmail.com>
2023-12-13 15:09:18 -0800

    build: add openssh client + sshpass (to support socks5 over ssh) build: use brave version from brave-version.txt bump brave version to 1.61.104

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export BROWSER=brave
-export BROWSER_VERSION=latest
+export BROWSER_VERSION=$(cat ./brave-version.txt)
 # build just for local platform
 docker buildx build --target=$BROWSER --build-arg BROWSER=$BROWSER --build-arg BROWSER_VERSION=$BROWSER_VERSION --progress plain --load -t webrecorder/browsertrix-browser-base:${BROWSER}-${BROWSER_VERSION} .
 
